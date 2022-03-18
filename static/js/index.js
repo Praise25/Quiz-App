2417c316bccb894227249b746757f2047b420728
Praise25 <anenepraise1@gmail.com>
2022-03-18 23:53:00 +0100

    fixed html entity display issue for the quiz answers

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,13 @@
 const questions = document.querySelectorAll(".question");
+const answers = document.querySelectorAll(".answer")
 for (let question of questions) {
   // retrieve and reassign innerHTML of the questions so the browser interperts html entities properly
   let originalText = question.innerText;
   question.innerHTML = originalText;
+}
+
+for (let answer of answers) {
+  // retrieve and reassign innerHTML of the answers so the browser interperts html entities properly
+  let originalText = answer.innerText;
+  answer.innerHTML = originalText;
 }
--- static/js/show.js
+++ static/js/show.js
@@ -1,4 +1,12 @@
 const question = document.querySelector("#question");
+const answers = document.querySelectorAll("button.answer");
+
 // retrieve and reassign innerHTML of the questions so the browser interperts html entities properly
 let originalQuestion = question.innerText;
 question.innerHTML = originalQuestion;
+
+for (let answer of answers) {
+  // retrieve and reassign innerHTML of the answers so the browser interperts html entities properly
+  let originalText = answer.innerText;
+  answer.innerHTML = originalText;
+}
