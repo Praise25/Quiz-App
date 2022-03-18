b71f6f790c473254ea2512904727b0f18ed5a475
Praise25 <anenepraise1@gmail.com>
2022-03-18 23:54:27 +0100

    added category and correctAnswer fields to the schema

--- models/quiz.js
+++ models/quiz.js
@@ -2,8 +2,10 @@ const mongoose = require("mongoose");
 
 const QuizSchema = new mongoose.Schema({
   serialNum: Number,
+  category: String,
   question: String,
   answers: Array,
+  correctAnswer: String,
 });
 
 module.exports = mongoose.model("Quiz", QuizSchema);
